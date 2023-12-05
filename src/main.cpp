5b596c6ad321e735a1c68a88e49a5db2e777b223
kukuikar <denis@perepetsky.ru>
2023-12-05 18:47:05 +0300

    main

--- src/main.cpp
+++ src/main.cpp
@@ -11,16 +11,16 @@ void sens()
 void setup() 
 {
 	Serial.begin(9600);  //открыть порт
-  pinMode(2, INPUT_PULLUP);   //2 пин как вход
-	attachInterrupt(0,sens,FALLING); //подключить прерывание на 2 пин при понижении сигнала
+	attachInterrupt(0, sens, FALLING); //подключить прерывание на 2 пин при понижении сигнала
 }
 
 void loop() 
 {
-  if ((micros() - lastflash) > 1000000)
+  if ((micros() - lastflash) > (250*1000))
   {          // если сигнала нет больше секунды
     RPM = 0; // считаем что RPM 0
   }
-  Serial.println(RPM); // вывод в порт
-  delay(50);           // задержка для стабильности
+
+  Serial.println(RPM);  // вывод в порт
+  delay(100);           // задержка для стабильности
 }
